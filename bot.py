0288623f669c54db2d372f1ebe72cc05dcf5899c
tamilmoviesdownload <ajayneymar14@gmail.com>
2026-01-02 18:26:46 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
